13c7efc05397c33e14810d2611bde1fb37b0b8fb
junpei <k1263j@gmail.com>
2019-04-02 02:33:27 +0900

    became generic

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,70 @@
-pub type DualNumber = (f64, f64);  // (real part, dual part)
+// f32 or f64
+
+extern crate num_traits;
+use num_traits::float::Float;
+
+pub type DualNumber<T> = (T, T);  // (real part, dual part)
 
 
 #[inline(always)]
-pub fn add(a: DualNumber, b: DualNumber) -> DualNumber {
+pub fn add<T: Float>(a: DualNumber<T>, b: DualNumber<T>) -> DualNumber<T> {
     (a.0 + b.0, a.1 + b.1)
 }
 
 #[inline(always)]
-pub fn mul(a: DualNumber, b: DualNumber) -> DualNumber {
+pub fn mul<T: Float>(a: DualNumber<T>, b: DualNumber<T>) -> DualNumber<T> {
     (a.0*b.0, a.0*b.1 + a.1*b.0)
 }
 
-/// "a" / "b"
+/// a / b
 #[inline(always)]
-pub fn div(a: DualNumber, b: DualNumber) -> DualNumber {
+pub fn div<T: Float>(a: DualNumber<T>, b: DualNumber<T>) -> DualNumber<T> {
     let real = a.0 / b.0;
     let dual = (b.0 * a.1 - b.1 * a.0) / (b.0 * b.0);
     (real, dual)
 }
 
 #[inline(always)]
-pub fn conj(a: DualNumber) -> DualNumber {
+pub fn conj<T: Float>(a: DualNumber<T>) -> DualNumber<T> {
     (a.0, -a.1)
 }
 
 #[inline(always)]
-pub fn inverse(a: DualNumber) -> DualNumber {
-    let real = 1.0 / a.0;
+pub fn inverse<T: Float>(a: DualNumber<T>) -> DualNumber<T> {
+    let one = T::one();
+
+    let real = one / a.0;
     let dual = -a.1 / (a.0 * a.0);
     (real, dual)
 }
 
 #[inline(always)]
-pub fn sqrt(a: DualNumber) -> DualNumber {
+pub fn sqrt<T: Float>(a: DualNumber<T>) -> DualNumber<T> {
+    let two = T::one() + T::one();
+
     let real = a.0.sqrt();
-    let dual = a.0 / ( 2.0 * a.1.sqrt() );
+    let dual = a.1 / ( two * a.0.sqrt() );
     (real, dual)
 }
 
 #[inline(always)]
-pub fn sin(a: DualNumber) -> DualNumber {
+pub fn sin<T: Float>(a: DualNumber<T>) -> DualNumber<T> {
     let real = a.0.sin();
     let dual = a.1 * a.0.cos();
     (real, dual)
 }
 
 #[inline(always)]
-pub fn cos(a: DualNumber) -> DualNumber {
+pub fn cos<T: Float>(a: DualNumber<T>) -> DualNumber<T> {
     let real = a.0.cos();
     let dual = -a.1 * a.0.sin();
     (real, dual)
+}
+
+#[inline(always)]
+pub fn exp<T: Float>(a: DualNumber<T>) -> DualNumber<T> {
+    let tmp = a.0.exp();
+    (tmp, tmp * a.1)
 }
 
 
