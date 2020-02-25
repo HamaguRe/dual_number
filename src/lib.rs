6dd8cb090cd4aec8ded157a4aabf300c48910979
junpei <k1263j@gmail.com>
2020-02-25 21:32:09 +0900

    added sub() and scale_add()

--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,30 @@ pub fn add(a: DualNumber<f64>, b: DualNumber<f64>) -> DualNumber<f64> {
 }
 
 #[inline(always)]
+pub fn sub(a: DualNumber<f64>, b: DualNumber<f64>) -> DualNumber<f64> {
+    (a.0 - b.0, a.1 - b.1)
+}
+
+#[inline(always)]
 pub fn scale(s: f64, d: DualNumber<f64>) -> DualNumber<f64> {
     (s * d.0, s * d.1)
 }
 
+/// s*a + b
+/// add( scale(s, a), b )
+#[inline(always)]
+pub fn scale_add(s: f64, a: DualNumber<f64>, b: DualNumber<f64>) -> DualNumber<f64> {
+    ( s.mul_add(a.0, b.0), s.mul_add(a.1, b.1) )
+}
+
+/// Calculate a*b (=b*a)
 #[inline(always)]
 pub fn mul(a: DualNumber<f64>, b: DualNumber<f64>) -> DualNumber<f64> {
     (a.0*b.0, a.0*b.1 + a.1*b.0)
 }
 
 /// a / b
+/// b の実部が0の場合には成立しない
 #[inline(always)]
 pub fn div(a: DualNumber<f64>, b: DualNumber<f64>) -> DualNumber<f64> {
     let real = a.0 / b.0;
@@ -31,17 +45,17 @@ pub fn conj(a: DualNumber<f64>) -> DualNumber<f64> {
     (a.0, -a.1)
 }
 
+/// 実部が0の場合には成立しない
 #[inline(always)]
-pub fn inverse(a: DualNumber<f64>) -> DualNumber<f64> {
-    let tmp = (a.0 * a.0).recip();
-    scale( tmp,  (a.0, -a.1) )
+pub fn inv(a: DualNumber<f64>) -> DualNumber<f64> {
+    conj(( a.0.recip(), a.1 / (a.0 * a.0) ))
 }
 
+/// 実部が0の場合には成立しない
 #[inline(always)]
 pub fn sqrt(a: DualNumber<f64>) -> DualNumber<f64> {
-    let real = a.0.sqrt();
-    let dual = a.1 / ( 2.0 * a.0.sqrt() );
-    (real, dual)
+    let tmp = a.0.sqrt();
+    ( tmp, a.1 / (2.0 * tmp) )
 }
 
 #[inline(always)]
@@ -60,13 +74,4 @@ pub fn cos(a: DualNumber<f64>) -> DualNumber<f64> {
 pub fn exp(a: DualNumber<f64>) -> DualNumber<f64> {
     let tmp = a.0.exp();
     (tmp, tmp * a.1)
-}
-
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
